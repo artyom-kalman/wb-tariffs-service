0be31ffa1f50c590ace8ef32c6e220d509585d39
artyom-kalman <p.a.kalashnikov@ya.ru>
2025-02-17 15:17:59 +1000

    Update main.ts

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,16 @@ async function main() {
 
   const urlData = require("../google_sheets_url.json");
 
-  try {
-    const tariffData = await fetchTariffData();
-    await insertOrUpdateTariffData(tariffData);
-    await exportToGoogleSheets(urlData.url);
-  } catch (error) {
-    console.error("Error running scheduled job:", error);
-  }
-
   console.log("Setting up scheduler");
   schedule.scheduleJob("0 * * * *", async () => {
     console.log(`Running scheduled job at ${new Date().toString()}`);
+    try {
+      const tariffData = await fetchTariffData();
+      await insertOrUpdateTariffData(tariffData);
+      await exportToGoogleSheets(urlData.url);
+    } catch (error) {
+      console.error("Error running scheduled job:", error);
+    }
   });
 }
 
